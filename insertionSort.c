8fd5c31bc1364a24d487ff5297ce5198876418e2
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-08-03 12:09:00 -0300

    Implementing the insertion sort

--- insertionSort.c
+++ insertionSort.c
@@ -18,3 +18,27 @@ int main (int argc, char * argv)
 
     return 0;
 }
+
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
